7d21f92507f26b38db2dbfd1d526f0de3fe19353
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-03-10 10:12:19 +0100

    Add a complete example of a simple postgres setup

---
+++ examples/simple-postgres/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = "1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0, <4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_availability_zones" "current" {}
+
+data "aws_vpc" "this" {
+  tags = {
+    Name = "shared"
+  }
+}
+
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+
+  tags = {
+    Tier = "Private"
+  }
+}
+
+module "database" {
+  source = "../../"
+
+  application_name = "tut-tut-tog"
+
+  engine         = "postgresql"
+  engine_version = "13"
+
+  availability_zones = data.aws_availability_zones.current.names
+  subnet_ids         = data.aws_subnets.private_subnets.ids
+  vpc_id             = data.aws_vpc.this.id
+}
