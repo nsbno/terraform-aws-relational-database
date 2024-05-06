6baad672226c47b75270cb5882ab752b1ad33bbd
faergestad <georg.f.persen@gmail.com>
2024-05-06 12:50:28 +0200

    Add rds-ca-rsa2048-g1 as default ca_cert_identifier

--- variables.tf
+++ variables.tf
@@ -176,5 +176,5 @@ variable "clone_from_existing_cluster_arn" {
 variable "ca_cert_identifier" {
   description = "Identifier of the CA certificate for the DB instance"
   type        = string
-  default     = "rds-ca-2019"
+  default     = "rds-ca-rsa2048-g1"
 }
